a681e44790c5211d4fe0266bdbc89c1b424b3526
Jonathan Juárez Valera <jjv20618@gmail.com>
2025-06-03 15:07:24 -0600

    Defer triggers added in defer blocks

--- src/app/dashboard/pages/defer-views/defer-views.component.ts
+++ src/app/dashboard/pages/defer-views/defer-views.component.ts
@@ -1,9 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { HeavyLoadersSlowComponent } from "../../../shared/components/heavy-loaders/heavy-loaders-slow.component";
+import { TitleComponent } from "../../../shared/components/title/title.component";
 
 @Component({
   selector: 'app-defer-views',
-  imports: [HeavyLoadersSlowComponent],
+  imports: [HeavyLoadersSlowComponent, TitleComponent],
   templateUrl: './defer-views.component.html',
   styles: `
     :host {
